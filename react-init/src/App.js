52022fb2eddae6e6576fa311534c71ebb340b0e0
king <102906132+Ggy-king@users.noreply.github.com>
2022-06-20 19:55:50 +0800

    Add files via upload

---
+++ react-init/src/App.js
@@ -0,0 +1,36 @@
+// import logo from './logo.svg';
+// 引入图片
+
+// import './App.css';
+// 页面的样式
+
+// App.js是书写页面的文件
+
+function App() {
+  return (
+    <div>
+      hello world
+    </div>
+
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
---
+++ react-init/src/App.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+// 这里是项目的自动化测试文件 
